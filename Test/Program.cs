b77d945eb2bb7772c206f471a3b95584059091b2
Yura_Filatov <yrafilatov@gmail.com>
2022-02-17 21:31:19 +0300

    Method PrintArray

---
+++ Test/Program.cs
@@ -0,0 +1,14 @@
+﻿string[] array = new string[] { "Vodka", "Матрешка", "балалайка", "3", "5", "3,14", "Пи", "int" };
+
+string PrintArray(string[] oldarray)
+{
+    string newarray = "[";
+    int count = oldarray.Length;
+    for (int i=0; i<count; i++)
+    {
+        newarray+= $"{oldarray[i]},";
+    }
+    newarray+= "]";
+    return newarray;
+}
+Console.WriteLine(PrintArray(array));
