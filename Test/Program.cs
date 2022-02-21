01a20f6107e0af4986c3a465be12d54a9cd9ac0b
Yura_Filatov <yrafilatov@gmail.com>
2022-02-21 22:42:22 +0300

    change method

--- Test/Program.cs
+++ Test/Program.cs
@@ -1,14 +1,20 @@
-﻿string[] array = new string[] { "Vodka", "Матрешка", "балалайка", "3", "5", "3,14", "Пи", "int" };
-
-string PrintArray(string[] oldarray)
+﻿string PrintArray(string[] oldarray)
 {
     string newarray = "[";
     int count = oldarray.Length;
-    for (int i=0; i<count; i++)
+    for (int i = 0; i < count; i++)
     {
-        newarray+= $"{oldarray[i]},";
+        if (oldarray[i].Length <= 3) 
+        {
+            if (i == oldarray.Length - 1) 
+                newarray += $"{oldarray[i]}";
+            else newarray+= $"{oldarray[i]},";
+        }
     }
-    newarray+= "]";
+    newarray += "]";
     return newarray;
 }
+
+string[] array = new string[] { "Vodka", "Матрешка", "балалайка", "3", "5", "3,14", "Пи", "int", "res" };
+
 Console.WriteLine(PrintArray(array));
